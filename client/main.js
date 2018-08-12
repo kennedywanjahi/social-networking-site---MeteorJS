6e57d439fa787906ba8ba54b39457198547a3422
ken <kennedywanjahi@gmail.com>
2018-08-12 11:16:08 +0300

    user management config and more tweaks

--- client/main.js
+++ client/main.js
@@ -3,6 +3,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 Post = new Mongo.Collection('post');
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_ONLY'
+})
+
 Template.postList.helpers({
   posts: function(){
     return Post.find();
